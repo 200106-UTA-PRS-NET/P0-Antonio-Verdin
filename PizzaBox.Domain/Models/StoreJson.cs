d36048891ebad5bcb6b1468dcb8b3007b320bebf
Tony <tony.verdin@gmail.com>
2020-01-12 11:35:45 -0600

    Testing Deserialization

---
+++ PizzaBox.Domain/Models/StoreJson.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PizzaBox.Domain.Models
+{
+   public class StoreJson
+    {
+      //  public int 
+    }
+}
--- PizzaBox/Program.cs
+++ PizzaBox/Program.cs
@@ -2,16 +2,42 @@
 using PizzaBox;
 using PizzaBox.Domain.Models;
 
+//testing namespaces
+using System.IO;
+using System.Text.Json;
+
 namespace PizzaBox
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Pizza pizza = new Pizza();
-            Pizza pizza2 = new Pizza();
-            pizza.showToppings();
 
+
+            string json = "";
+
+            //Testing Code Below
+            //Pizza pizza = new Pizza();
+            //Pizza pizza2 = new Pizza();
+            //pizza.showToppings();
+            //using (StreamReader r = new StreamReader("C:\\Users\\theki\\Documents\\Revature\\projects\\P0-Antonio-Verdin\\PizzaBox.Storing\\Repositories\\Stores.json"))
+            //using (StreamReader r = new StreamReader("..\\P0-Antonio-Verdin\\PizzaBox.Storing\\Repositories\\Stores.json"))
+            {
+              // json = r.ReadToEnd();
+                
+            }
+            TempStore stores = new TempStore();
+            string jsonString = File.ReadAllText("C:\\Users\\theki\\Documents\\Revature\\projects\\P0-Antonio-Verdin\\PizzaBox.Storing\\Repositories\\Stores.json");
+            stores = JsonSerializer.Deserialize<TempStore>(jsonString);
+
+
+            
         }
+    }
+    class TempStore
+    {
+        public string Name { get; set;}
+        public int Id { get; set; } 
+        
     }
 }
